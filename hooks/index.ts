58cf862d99d5b016d3959e0fa0867e5360d8ca40
Zmin2003 <bantianjingchuan@gmail.com>
2026-02-21 09:15:42 +0800

    refactor: 清理未使用组件与遗留 hooks

--- hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useTheme } from './useTheme';
-export { useLocalStorage } from './useLocalStorage';
-export { useAuth } from './useAuth';
-export { useAppData } from './useAppData';
-export { useSearch } from './useSearch';
-export { useContextMenu } from './useContextMenu';
-export { safeHostname, ensureProtocol, compareByOrder, normalizeDomain } from './useAppData';
-export type { SyncStatus } from './useAppData';
